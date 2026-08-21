3334d9ba28123c613997ba14b35b6cf85e0dbcb6
hapidltf <141009329+hapidltf@users.noreply.github.com>
2026-08-21 11:13:28 +0700

    Add files via upload
    upload praktikum identitas

---
+++ identitas.py
@@ -0,0 +1,7 @@
+print("\n");
+print("\t--- Identitas ---");
+print("\n Nama: Muhammad Hafidz Luthfi");
+print("\n NIM: 2605060022");
+print("\n TTL: 16 Februari 2008");
+print("\n Hobi: Main Game");
+print("\n");
